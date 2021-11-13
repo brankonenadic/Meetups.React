4f97ba180c2560955da33001216c50e4395ad200
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-11-13 22:37:36 +0100

    Update

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
 const Todo = (props) => {
+    const dedleteHandler = () => {
+        console.log(props.text);
+    }
     return (
         <div className="card">
             <h2>{props.text}</h2>
             <div className="actions">
-                <button className="btn">Delete</button>
+                <button className="btn" onClick={dedleteHandler}>Delete</button>
             </div>
         </div>
     );
