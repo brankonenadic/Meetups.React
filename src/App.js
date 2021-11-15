958cf9f89ad05e1fa2718333270ec9a6aaf672d4
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-11-15 22:24:19 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import { Route, Switch } from "react-router-dom";
 import AllMeetups from "./pages/AllMeetups";
 import Favorites from "./pages/Favorites";
 import NewMeetup from "./pages/NewMeetup";
-
+import Navigation from './layout/Navigation';
 
 function App() {
   return <div>
+    <Navigation />
     <Switch>
-    <Route path="/">
+    <Route path="/" exact>
       <AllMeetups />
     </Route>
     <Route path="/new-meetups">
