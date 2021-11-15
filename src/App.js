dacc921319a9e12e07f625e91531f5d02e90a72c
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-11-15 21:34:12 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import AllMeetups from "./pages/AllMeetups";
+import Favorites from "./pages/Favorites";
+import NewMeetup from "./pages/NewMeetup";
+
+
 function App() {
-  return <div></div>;
+  return <div>
+    <Route path="/all-meetups">
+      <AllMeetups />
+    </Route>
+    <Route path="/new-meetups">
+      <NewMeetup />
+    </Route>
+    <Route path="/favorites">
+      <Favorites />
+    </Route>
+  </div>;
 }
 
 export default App;
