902a078e66fd07d0a1886de7cd090ff73dd2f7ba
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-11-15 21:10:26 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,5 @@
-
-import Todo from "./components/Todo";
-
-
 function App() {
-  return <div>
-    <h1>My Todos</h1>
-    <Todo text="Learn React"/>
-    <Todo text="Learn JavaScript" />
-    <Todo text="Learn PHP" />
-    <Todo text="Learn SQL" />
-  </div>;
+  return <div></div>;
 }
 
 export default App;
