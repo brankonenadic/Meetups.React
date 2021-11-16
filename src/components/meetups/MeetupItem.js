b091c826979ad291e79a127112334dcd7fac120d
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-11-16 21:12:33 +0100

    Update

--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React from 'react';
+import classes from './MeetupsItem.module.css';
 
-const MeetupItem = () => {
+
+const MeetupItem = (props) => {
     return (
-        <div>
-            
+        <li className={classes.item}>
+        <div className={classes.image}>
+            <img src={props.image} alt={props.title} />
         </div>
+        <div className={classes.content}>
+            <h2>{props.title}</h2>
+            <address>{props.address}</address>
+            <p>{props.description}</p>
+        </div>
+        <div className={classes.actions}>
+            <button>To favorites</button>
+        </div>
+    </li>
     )
 }
 
--- src/components/meetups/MeetupsList.js
+++ src/components/meetups/MeetupsList.js
@@ -2,15 +2,7 @@ import React from 'react';
 
 const MeetupsList = (props) => {
     return (
-        <li>
-            <div>
-                <img src={props.image} alt={props.title} />
-            </div>
-            <div>
-                <h2>{props.title}</h2>
-                <address>{props.address}</address>
-            </div>
-        </li>
+        <div></div>
     )
 }
 
