a302eeb8c2438d5f1c4c028db617022dec9a3712
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-11-17 20:31:16 +0100

    Update

--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import classes from './MeetupsItem.module.css';
-
+import Card from '../ui/Card';
 
 const MeetupItem = (props) => {
     return (
         <li className={classes.item}>
-        <div className={classes.image}>
-            <img src={props.image} alt={props.title} />
-        </div>
-        <div className={classes.content}>
-            <h2>{props.title}</h2>
-            <address>{props.address}</address>
-            <p>{props.description}</p>
-        </div>
-        <div className={classes.actions}>
-            <button>To favorites</button>
-        </div>
-    </li>
+            <Card>
+                <div className={classes.image}>
+                    <img src={props.image} alt={props.title} />
+                </div>
+                <div className={classes.content}>
+                    <h2>{props.title}</h2>
+                    <address>{props.address}</address>
+                    <p>{props.description}</p>
+                </div>
+                <div className={classes.actions}>
+                    <button>To favorites</button>
+                </div>
+            </Card>
+        </li>
     )
 }
 
