c74764d2bc6d439c1f69d43cd94895fd9a0df7f4
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-11-17 20:50:50 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React from "react";
+import { Fragment } from "react";
 import { Route, Switch } from "react-router-dom";
+import Layout from "./components/layout/Layout";
 import AllMeetups from "./pages/AllMeetups";
 import Favorites from "./pages/Favorites";
 import NewMeetup from "./pages/NewMeetup";
-import Navigation from './components/layout/Navigation';
+
 
 function App() {
-  return <div>
-    <Navigation />
-    <Switch>
-    <Route path="/" exact>
-      <AllMeetups />
-    </Route>
-    <Route path="/new-meetups">
-      <NewMeetup />
-    </Route>
-    <Route path="/favorites">
-      <Favorites />
-    </Route>
-    </Switch>
-  </div>;
+  return (
+    <Fragment>
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetups />
+        </Route>
+        <Route path="/new-meetups">
+          <NewMeetup />
+        </Route>
+        <Route path="/favorites">
+          <Favorites />
+        </Route>
+      </Switch>
+    </Layout>
+    </Fragment>
+  );
 }
 
 export default App;
