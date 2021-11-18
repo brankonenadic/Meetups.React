f3553710ae5f6030fc57e40ba97472909b12a6c0
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-11-18 22:18:11 +0100

    Update

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -5,7 +5,12 @@ import Card from '../ui/Card';
 const NewMeetupForm = () => {
     return (
         <Card>
-
+            <form className={classes.form}>
+                <div className={classes.control}>
+                    <label htmlFor="title">Meetup Title</label>
+                    <input type="text" id="title" required />
+                </div>
+            </form>
         </Card>
     );
 }
