ed3f36cae5cdc5428b952ee237cc38b6dbd46b1f
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-11-18 22:23:13 +0100

    Update

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -10,6 +10,18 @@ const NewMeetupForm = () => {
                     <label htmlFor="title">Meetup Title</label>
                     <input type="text" id="title" required />
                 </div>
+                <div className={classes.control}>
+                    <label htmlFor="image">Image</label>
+                    <input type="url" id="image" required />
+                </div>
+                <div className={classes.control}>
+                    <label htmlFor="address">Address</label>
+                    <input type="text" id="address" required />
+                </div>
+                <div className={classes.control}>
+                    <label htmlFor="description">Description</label>
+                    <textarea type="text" id="description" required rows="5" ></textarea>
+                </div>
             </form>
         </Card>
     );
