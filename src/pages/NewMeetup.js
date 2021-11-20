5275dc56166c7e7586c22bc2ef8696d03d8ab01f
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-11-20 21:38:04 +0100

    Update

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -6,7 +6,10 @@ const NewMeetup = () => {
         fetch('https://meetups-b5772-default-rtdb.europe-west1.firebasedatabase.app/meetups.json', 
         {
             method: 'POST',
-            
+            body: JSON.stringify(meetupData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
         })
     }
     return (
