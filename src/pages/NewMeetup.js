7e8721d9dae348c1eaf2da5cc6e7d78ddf1b3c36
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-11-22 22:26:31 +0100

    Update

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import NewMeetupForm from '../components/meetups/NewMeetupForm';
+import { useHistory } from 'react-router-dom';
+
 
 const NewMeetup = () => {
+const history = useHistory();
+
     const addMeetupHandler = (meetupData) => {
         fetch('https://meetups-b5772-default-rtdb.europe-west1.firebasedatabase.app/meetups.json',
             {
