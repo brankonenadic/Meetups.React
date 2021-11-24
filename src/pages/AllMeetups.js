1fa565a4a02c2e29c65420cf2928a92b0ff7eb23
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-11-24 19:58:37 +0100

    Update

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import MeetupsList from '../components/meetups/MeetupsList';
 
 
@@ -7,17 +7,34 @@ const AllMeetups = () => {
   const [isLoading, setisLoading] = useState(true);
   const [loadedData, setloadedData] = useState([]);
 
-  fetch('https://meetups-b5772-default-rtdb.europe-west1.firebasedatabase.app/meetups.json').then((response) => {
-    response.json();
-  }).then((data) => {
-    setisLoading(false);
-    setloadedData(data);
-  });
+
+  useEffect(() => {
+    setisLoading(true);
+
+    fetch('https://meetups-b5772-default-rtdb.europe-west1.firebasedatabase.app/meetups.json').then((response) => {
+      return response.json();
+    }).then((data) => {
+      const meetups = [];
+      for (const key in data) {
+        const meetup = {
+          id: key,
+          ...data[key]
+        };
+        meetups.push(meetup);
+      }
+
+      setisLoading(false);
+      setloadedData(meetups);
+    });
+  }, []);
+
+
   if (isLoading) {
     return (
       <p>Loading...</p>
     );
   };
+  console.log(loadedData);
 
   return (
     <div>
