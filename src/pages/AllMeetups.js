b43e63bb2753779e8f09372c43c1445dfa74c195
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-11-24 19:29:35 +0100

    Update

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,26 +1,7 @@
 import React, {useState} from 'react';
 import MeetupsList from '../components/meetups/MeetupsList';
 
-const DUMMY_DATA = [
-  {
-    id: 'm1',
-    title: 'This is a first meetup',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-    address: 'Meetupstreet 5, 12345 Meetup City',
-    description:
-      'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-  },
-  {
-    id: 'm2',
-    title: 'This is a second meetup',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-    address: 'Meetupstreet 5, 12345 Meetup City',
-    description:
-      'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-  },
-];
+
 
 const AllMeetups = () => {
   const [isLoading, setisLoading] = useState(true);
@@ -30,11 +11,17 @@ const AllMeetups = () => {
     response.json();
   }).then((data) => {
     setisLoading(false);
+    setloadedData(data);
   });
+  if (isLoading) {
+    return (
+      <p>Loading...</p>
+    );
+  };
 
   return (
     <div>
-      <MeetupsList meetups={DUMMY_DATA} />
+      <MeetupsList meetups={loadedData} />
     </div>
   )
 }
