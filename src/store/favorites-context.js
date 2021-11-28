9ea86b0acabefb645ad03807de32242f7d6c5704
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-11-28 10:26:34 +0100

    Update

--- src/store/favorites-context.js
+++ src/store/favorites-context.js
@@ -1,4 +1,17 @@
 import { createContext } from "react";
 
 
-const FavoriteContext = createContext();
+const FavoriteContext = createContext({
+    favorites: [],
+    totalFavorites: 0
+});
+
+const FavoritesContextProvider = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default FavoritesContextProvider;
